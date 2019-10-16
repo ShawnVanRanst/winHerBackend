ed1dd3de9b5fc980a6cdf20ebca1fff3ce2bbda2
ArnoBoel <arno-boel@hotmail.be>
2019-10-16 12:09:32 +0200

    Add passenger and flightcrew model

---
+++ OnBoardFlight/OnBoardFlight/Models/FlightCrew.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight.Models
+{
+    public class FlightCrew
+    {
+
+        #region Properties
+
+        public string UserName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+
+        #endregion
+
+        #region Constructors
+
+        public FlightCrew()
+        {
+        }
+
+        public FlightCrew(string userName, string firstName, string lastName)
+        {
+            UserName = userName;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
---
+++ OnBoardFlight/OnBoardFlight/Models/Passenger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnBoardFlight.Models
+{
+    public class Passenger
+    {
+
+        #region Properties
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Seat { get; set; }
+
+
+
+        #endregion
+
+        #region Constructors
+
+        public Passenger()
+        {
+        }
+
+        public Passenger(string firstName, string lastName, string seat)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Seat = seat;
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
